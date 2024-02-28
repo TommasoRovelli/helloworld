031ec74a5191e2d5133e9957ed90dd05540158b4
TommasoRovelli <tommaso.rovelli@yahoo.it>
2024-02-28 12:12:56 +0100

    new feature

--- src/helloworld.nim
+++ src/helloworld.nim
@@ -1,5 +1,10 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
+import os
 
 when isMainModule:
-  echo("Hello, World!")
+
+  if commandLineParams().len != 0:
+    echo "Hello, ", commandLineParams()[0], "!"
+  else:
+    echo "Hello, World!"
